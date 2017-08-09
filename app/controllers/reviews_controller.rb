a067c5a70ae2c87ea8df1921561014294f467637
Romain Neuville <neuvilleromain@gmail.com>
2017-08-09 15:16:17 +0200

    Finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,23 @@
 class ReviewsController < ApplicationController
-#   def new
-#     @review = Review.new
-#   end
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
 
-#   def create
-#     @review = Review.new(restaurants_params)
-#     if @review.save
-#       redirect_to restaurant_path
-#     else
-#       render :new
-#     end
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant_id)
+    else
+      @restaurant = Restaurant.find(params[:restaurant_id])
+      render :new
+    end
+  end
 
-#   end
-
-# private
-
-#   def reviews_params
-#     params.require(:reviews).permit(:content, :)
-#   end
+private
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 
 end
